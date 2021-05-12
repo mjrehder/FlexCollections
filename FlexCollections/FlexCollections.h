074898a2c73d1087b540dbcd99b6e7e5ceca7478
Martin Jacob Rehder <gitrepocon01@rehsco.com>
2021-05-12 12:50:14 +0200

    Initial Commit

---
+++ FlexCollections/FlexCollections.h
@@ -0,0 +1,18 @@
+//
+//  FlexCollections.h
+//  FlexCollections
+//
+//  Created by Martin Rehder on 12.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlexCollections.
+FOUNDATION_EXPORT double FlexCollectionsVersionNumber;
+
+//! Project version string for FlexCollections.
+FOUNDATION_EXPORT const unsigned char FlexCollectionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexCollections/PublicHeader.h>
+
+
